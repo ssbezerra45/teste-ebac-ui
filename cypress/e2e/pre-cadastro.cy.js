fb81b5ce9241cf14e26f29a3539a3c7ef37b07bc
sergiobezerra <ssbezerra45@gmail.com>
2026-04-04 23:14:26 -0300

    pre cadastro

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -1,18 +1,29 @@
 /// <reference types="cypress" />
+import { faker } from '@faker-js/faker';
 describe('Funcionalidade Pré cadastro', () => {        
 
     beforeEach(() => {
         cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/")
     });
     it('Deve completar o pré-cadastro com sucesso', () => {
-        cy.get('[name="email"]').type("aluno_ebac99@teste.com")
-        cy.get('.register > :nth-child(2) > [name="password"]').type("!teste1234@teste.com")
+        let emailFaker = faker.internet.email()
+        let passwordFaker = faker.internet.password()
+      
+
+        cy.get('[name="email"]').type(emailFaker)
+        cy.get('.register > :nth-child(2) > [name="password"]').type(passwordFaker)
         cy.get('[name="register"]').click()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type("sergio")
-        cy.get('#account_last_name').type("bezerra")
+        cy.get('[name="account_first_name"]').type('Sergio') 
+        cy.get('[name="account_last_name"]').type('Santos')
         cy.get('.woocommerce-Button').click()
-        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+        cy.get('[name="save_account_details"]').should('contain', 'Detalhes da conta modificados com sucesso.')
+
+
+
+
+     
+       
     });
 });
